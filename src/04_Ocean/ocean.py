d57784b949a121c1a1f08337671e3338fce1a3a3
wicktosha1 <152454405+wicktosha1@users.noreply.github.com>
2024-01-18 11:24:52 +1000

    add ocean

--- src/04_Ocean/ocean.py
+++ src/04_Ocean/ocean.py
@@ -9,16 +9,64 @@ class Ocean:
         self.state = init_state
 
     def __str__(self) -> str:
-        return "\n".join(["".join(str(el) for el in row) for row in self.state])
+        return "\n".join([" ".join(str(el) for el in row) for row in self.state])
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self.state!r})"
 
     def gen_next_quantum(self) -> "Ocean":
-        raise NotImplementedError
+        new_state = []
+        fish = 2
+        shrimp = 3
+
+        for i in range(len(self.state)):
+            new_row = []
+            for j in range(len(self.state[i])):
+                if self.state[i][j] == 1:
+                    new_row.append(1)
+                else:
+                    n_fish = 0
+                    n_shrimp = 0
+                    neighbors = [
+                        (i - 1, j - 1),
+                        (i - 1, j),
+                        (i - 1, j + 1),
+                        (i, j - 1),
+                        (i, j + 1),
+                        (i + 1, j - 1),
+                        (i + 1, j),
+                        (i + 1, j + 1),
+                    ]
+                    for ni, nj in neighbors:
+                        if ni < 0 or nj < 0 or ni >= len(self.state) or nj >= len(self.state[i]):
+                            continue
+                        if self.state[ni][nj] == fish:
+                            n_fish += 1
+                        elif self.state[ni][nj] == shrimp:
+                            n_shrimp += 1
+
+                    if self.state[i][j] == fish:
+                        if n_fish < 2 or n_fish > 3:  # noqa: PLR2004
+                            new_row.append(0)
+                        else:
+                            new_row.append(2)
+                    elif self.state[i][j] == shrimp:
+                        if n_shrimp < 2 or n_shrimp > 3:  # noqa: PLR2004
+                            new_row.append(0)
+                        else:
+                            new_row.append(3)
+                    elif n_fish == fish and n_shrimp == shrimp:
+                        new_row.append(2)
+                    elif n_fish == 3:  # noqa: PLR2004
+                        new_row.append(2)
+                    else:
+                        new_row.append(0)
+            new_state.append(new_row)
+
+        return Ocean(init_state=new_state)
 
 
-if __name__ == "__main__":
+if name == "__main__":
     n_quantums = int(sys.stdin.readline())
     n_rows, n_clms = (int(i) for i in sys.stdin.readline().split())
     init_state = []
@@ -29,4 +77,4 @@ if __name__ == "__main__":
     ocean = Ocean(init_state=init_state)
     for _ in range(n_quantums):
         ocean = ocean.gen_next_quantum()
-    print(ocean)
+    print(ocean)  # noqa: T201
--- src/04_Ocean/test_ocean.py
+++ src/04_Ocean/test_ocean.py
@@ -1,14 +1,36 @@
+from typing import ClassVar  # noqa: I001
 from ocean import Ocean
 
 
-def test_ocean_init():
-    Ocean
-    assert False
+class TestOcean:
+    init_state: ClassVar[list[list[int]]] = [
+        [0, 2, 0, 1, 3],
+        [0, 2, 0, 3, 3],
+        [0, 2, 2, 2, 1],
+        [2, 1, 2, 0, 3],
+        [3, 3, 1, 3, 3],
+    ]
 
+    next_state: ClassVar[list[list[int]]] = [
+        [0, 0, 0, 1, 3],
+        [2, 2, 0, 3, 3],
+        [2, 0, 0, 2, 1],
+        [0, 1, 2, 2, 3],
+        [0, 0, 1, 3, 3],
+    ]
 
-def test_ocean_repr():
-    assert False
+    ocean = Ocean(init_state)
 
+    def test_ocean_init(self):
+        assert str(self.ocean) == "\n".join(
+            [" ".join(str(el) for el in row) for row in self.init_state]
+        )
 
-def test_ocean_step():
-    assert False
+    def test_ocean_repr(self):
+        assert self.ocean.__repr__() == f"Ocean({self.init_state!r})"
+
+    def test_ocean_step(self):
+        self.ocean = self.ocean.gen_next_quantum()
+        assert str(self.ocean) == "\n".join(
+            [" ".join(str(el) for el in row) for row in self.next_state]
+        )
